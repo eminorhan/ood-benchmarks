7777f26d6e358715d4d47580eff52b9d7ac51ba8
eminorhan <eo41@nyu.edu>
2020-02-20 23:50:12 -0500

    Add more adversarial robustness results, update README

--- scripts/evaluate_whitebox.sh
+++ scripts/evaluate_whitebox.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:1
 ##SBATCH --cpus-per-task=1
 #SBATCH --mem=100GB
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --array=0
 #SBATCH --job-name=whi
 #SBATCH --output=whi_%A_%a.out
@@ -15,7 +15,7 @@ module purge
 module load cuda-10.0
 source /home/eo41/venv/bin/activate
 
-#python -u /misc/vlgscratch4/LakeGroup/emin/oos_benchmarks/evaluate_whitebox.py '/misc/vlgscratch4/LakeGroup/emin/robust_vision/imagenet/' --model-name 'tf_efficientnet_l2_ns_475' --im-size 475
+python -u /misc/vlgscratch4/LakeGroup/emin/oos_benchmarks/evaluate_whitebox.py '/misc/vlgscratch4/LakeGroup/emin/robust_vision/imagenet/' --model-name 'tf_efficientnet_l2_ns_475' --im-size 475
 #python -u /misc/vlgscratch4/LakeGroup/emin/oos_benchmarks/evaluate_whitebox.py '/misc/vlgscratch4/LakeGroup/emin/robust_vision/imagenet/' --model-name 'tf_efficientnet_b7_ns' --im-size 600
 #python -u /misc/vlgscratch4/LakeGroup/emin/oos_benchmarks/evaluate_whitebox.py '/misc/vlgscratch4/LakeGroup/emin/robust_vision/imagenet/' --model-name 'tf_efficientnet_b6_ns' --im-size 528
 #python -u /misc/vlgscratch4/LakeGroup/emin/oos_benchmarks/evaluate_whitebox.py '/misc/vlgscratch4/LakeGroup/emin/robust_vision/imagenet/' --model-name 'tf_efficientnet_b5_ns' --im-size 456
@@ -33,6 +33,6 @@ source /home/eo41/venv/bin/activate
 #python -u /misc/vlgscratch4/LakeGroup/emin/oos_benchmarks/evaluate_whitebox.py '/misc/vlgscratch4/LakeGroup/emin/robust_vision/imagenet/' --model-name 'tf_efficientnet_b3' --im-size 300
 #python -u /misc/vlgscratch4/LakeGroup/emin/oos_benchmarks/evaluate_whitebox.py '/misc/vlgscratch4/LakeGroup/emin/robust_vision/imagenet/' --model-name 'tf_efficientnet_b2' --im-size 260
 #python -u /misc/vlgscratch4/LakeGroup/emin/oos_benchmarks/evaluate_whitebox.py '/misc/vlgscratch4/LakeGroup/emin/robust_vision/imagenet/' --model-name 'tf_efficientnet_b1' --im-size 240
-python -u /misc/vlgscratch4/LakeGroup/emin/oos_benchmarks/evaluate_whitebox.py '/misc/vlgscratch4/LakeGroup/emin/robust_vision/imagenet/' --model-name 'tf_efficientnet_b0' --im-size 224
+#python -u /misc/vlgscratch4/LakeGroup/emin/oos_benchmarks/evaluate_whitebox.py '/misc/vlgscratch4/LakeGroup/emin/robust_vision/imagenet/' --model-name 'tf_efficientnet_b0' --im-size 224
 
 echo "Done"
